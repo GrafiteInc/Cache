5ef245307dc5457e49923165f0bb1ca3699bf25f
Matt Lantz <mattlantz@gmail.com>
2025-01-28 11:37:25 -0500

    Update CreateCacheDatabase.php

--- src/Commands/CreateCacheDatabase.php
+++ src/Commands/CreateCacheDatabase.php
@@ -3,6 +3,7 @@
 namespace Grafite\Cache\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Config;
 
 class CreateCacheDatabase extends Command
 {
@@ -18,7 +19,7 @@ class CreateCacheDatabase extends Command
      *
      * @var string
      */
-    protected $description = 'Create an sqlite cache database.';
+    protected $description = 'Create a cache database.';
 
     /**
      * Execute the console command.
@@ -31,9 +32,20 @@ class CreateCacheDatabase extends Command
 
         if (! file_exists(database_path('cache.sqlite'))) {
             touch(database_path('cache.sqlite'));
-            // Set the table
-            app('db')->connection('sqlite_cache')->statement('CREATE TABLE cache (key STRING PRIMARY KEY, value LONGTEXT, expiration INTEGER)');
         }
+
+        \sleep(1);
+
+         Config::set('database.connections.sqlite_cache', [
+            'driver' => 'sqlite',
+            'database' => database_path('cache.sqlite'),
+            'prefix' => config('cache.stores.sqlite.prefix'),
+        ]);
+
+        sleep(1);
+
+        // Set the table
+        app('db')->connection('sqlite_cache')->statement('CREATE TABLE cache (key STRING PRIMARY KEY, value LONGTEXT, expiration INTEGER)');
 
         return 0;
     }
