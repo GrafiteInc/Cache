0e298149a690182abe750e9eb7bcadfab6da996b
Matt Lantz <mattlantz@gmail.com>
2025-02-20 14:54:47 -0500

    because of tests

--- tests/Unit/CacheTest.php
+++ tests/Unit/CacheTest.php
@@ -10,8 +10,12 @@ class CacheTest extends TestCase
 {
     public function testCacheProvider()
     {
+        if (file_exists(database_path('cache.sqlite'))) {
+            unlink(database_path('cache.sqlite'));
+        }
+
         // Create the database
-        Artisan::call('make:cache:database');
+        Artisan::call('make:cache-database');
 
         // Put a value
         Cache::put('foo', 'bar', 60);
@@ -21,10 +25,14 @@ class CacheTest extends TestCase
         $this->assertEquals('bar', $value);
     }
 
-        public function testCacheKeyMaker()
+    public function testCacheKeyMaker()
     {
+        if (file_exists(database_path('cache.sqlite'))) {
+            unlink(database_path('cache.sqlite'));
+        }
+
         // Create the database
-        Artisan::call('make:cache:database');
+        Artisan::call('make:cache-database');
 
         // Put a value
         $key = cache()->key('foo', ['bar']);
@@ -34,5 +42,28 @@ class CacheTest extends TestCase
         $value = Cache::get($key);
 
         $this->assertEquals('bar', $value);
+    }
+
+    public function testCacheLocksTable()
+    {
+        if (file_exists(database_path('cache.sqlite'))) {
+            unlink(database_path('cache.sqlite'));
+        }
+
+        // Create the database
+        Artisan::call('make:cache-database');
+
+        // Create the lock table
+        Artisan::call('make:cache-locks-table');
+
+        $lock = cache()->lock('foo-bar', 10);
+        $value = 'bar';
+
+        if ($lock->get()) {
+            // Lock acquired for 10 seconds...
+            $this->assertEquals('bar', $value);
+
+            $lock->release();
+        }
     }
 }
